af38d930a1e4749fc52b356557585603f1908fcd
Ajay <ajay1212ee@gmail.com>
2024-07-09 18:36:20 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-65nwq5chr-ajaygupta1822s-projects.vercel.app/',
     credentials:true
 };
 app.use(cors(corsOption));
